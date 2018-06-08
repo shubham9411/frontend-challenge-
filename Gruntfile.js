babcede61f18a1f5003297900b4b46d1253b66ae
Shubham Pandey <shubham9411@gmail.com>
2018-06-08 09:56:31 +0530

    Automation setup

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		// Watch task config
+		watch: {
+			options: {
+				livereload: true
+			},
+			sass: {
+				files: "src/scss/*.scss",
+				tasks: ['sass']
+			},
+			scripts: {
+				files: "src/js/*.js",
+				tasks: ['uglify']
+			}
+		},
+		// SASS task config
+		sass: {
+			dev: {
+				files: {
+					// destination         // source file
+					"dist/css/style.css": "src/scss/style.scss"
+				}
+			}
+		},
+		browserSync: {
+			default_options: {
+				bsFiles: {
+					src: [
+						"src/scss/*.scss",
+						"src/js/*.js",
+						"*.html"
+					]
+				},
+				options: {
+					watchTask: true,
+					server: {
+						baseDir: "./"
+					}
+				}
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'dist/js/main.min.js': ['src/js/main.js']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['browserSync', 'watch']);
+};
